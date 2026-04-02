2d00194a555d940e9002ec54c26eff698df72b86
chrvnixx <emmanueladekola91@gmail.com>
2026-04-02 09:58:53 +0100

    delete product controller

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -44,7 +44,7 @@ export async function createProduct(req, res) {
 
     if (image) {
       cloudinaryResponse = await cloudinary.uploader.upload(image, {
-        folder: "Product",
+        folder: "Products",
       });
     }
 
@@ -62,5 +62,32 @@ export async function createProduct(req, res) {
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
     console.log("Error in create product controller");
+  }
+}
+
+export async function deleteProduct(req, res) {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(400).json({ message: "Product not found" });
+    }
+
+    if (product.image) {
+      const publicId = product.image.split("/").pop().split(".")[0];
+      try {
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+        console.log("Image deleted from cloudinary");
+      } catch (error) {
+        console.log("Error deleting image from cloudinary");
+      }
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "product deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+    console.log("Error in delete product controller");
   }
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { createProduct, getAllProducts, getFeaturedProducts } from "../controllers/productControllers.js"
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts } from "../controllers/productControllers.js"
 import { adminRoute, protectRoute } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.get("/", protectRoute, adminRoute, getAllProducts)
 router.get("/featured",  getFeaturedProducts)
-router.post("/create-product",  createProduct)
+router.post("/create-product", protectRoute, adminRoute, createProduct)
+router.delete("/create-product/:id", protectRoute, adminRoute, deleteProduct)
 
 export default router
